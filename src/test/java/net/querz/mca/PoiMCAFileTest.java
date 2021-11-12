8ea39c1071f743f334cb4af3216112ca2e2efbee
BuildTools <unconfigured@null.spigotmc.org>
2021-11-12 15:47:20 -0500

    Add POI read/write parity tests for all supported versions

--- src/test/java/net/querz/mca/PoiMCAFileTest.java
+++ src/test/java/net/querz/mca/PoiMCAFileTest.java
@@ -7,6 +7,26 @@ import java.util.stream.Collectors;
 
 public class PoiMCAFileTest extends MCAFileBaseTest {
 
+    public void testMcaReadWriteParity_1_14_4() {
+        validateReadWriteParity(DataVersion.JAVA_1_14_4, "1_14_4/poi/r.-1.0.mca", PoiMCAFile.class);
+    }
+
+    public void testMcaReadWriteParity_1_15_2() {
+        validateReadWriteParity(DataVersion.JAVA_1_15_2, "1_15_2/poi/r.-1.0.mca", PoiMCAFile.class);
+    }
+
+    public void testMcaReadWriteParity_1_16_5() {
+        validateReadWriteParity(DataVersion.JAVA_1_16_5, "1_16_5/poi/r.0.-1.mca", PoiMCAFile.class);
+    }
+
+    public void testMcaReadWriteParity_1_17_1() {
+        validateReadWriteParity(DataVersion.JAVA_1_17_1, "1_17_1/poi/r.-3.-2.mca", PoiMCAFile.class);
+    }
+
+    public void testMcaReadWriteParity_1_18_PRE1() {
+        validateReadWriteParity(DataVersion.JAVA_1_18_PRE1, "1_18_PRE1/poi/r.-2.-3.mca", PoiMCAFile.class);
+    }
+
     public void testPoiMca_1_17_1() {
         PoiMCAFile mca = assertThrowsNoException(() -> MCAUtil.readAuto(copyResourceToTmp("1_17_1/poi/r.-3.-2.mca")));
         PoiChunk chunk = mca.stream().filter(Objects::nonNull).findFirst().orElse(null);
@@ -25,9 +45,5 @@ public class PoiMCAFileTest extends MCAFileBaseTest {
         assertEquals(new PoiRecord(-1032, 63, -670, "minecraft:home"), recordsByType.get("minecraft:home").get(0));
         // it'd be better if we had a bell in this chunk to test a non-zero value here
         assertEquals(0, recordsByType.get("minecraft:home").get(0).getFreeTickets());
-    }
-
-    public void testMcaReadWriteParity_1_17_1() {
-        validateReadWriteParity(DataVersion.JAVA_1_17_1, "1_17_1/poi/r.-3.-2.mca", PoiMCAFile.class);
     }
 }
