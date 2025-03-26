562c4eff558054e4a60e86d099df3f61bb42a47f
ens-gijs <ens.gijs@gmail.com>
2025-03-25 21:36:29 -0400

    Add DataVersion 1.21.5

--- src/main/java/io/github/ensgijs/nbt/mca/DataVersion.java
+++ src/main/java/io/github/ensgijs/nbt/mca/DataVersion.java
@@ -815,7 +815,8 @@ public enum DataVersion {
     JAVA_1_21_5_PRE2(4321, 21, 5, "PRE2"),
     JAVA_1_21_5_PRE3(4322, 21, 5, "PRE3"),
     JAVA_1_21_5_RC1(4323, 21, 5, "RC1"),
-    JAVA_1_21_5_RC2(4324, 21, 5, "RC2"),;
+    JAVA_1_21_5_RC2(4324, 21, 5, "RC2"),
+    JAVA_1_21_5(4325, 21, 5),;
 
     private static final int[] ids;
     private static final DataVersion latestFullReleaseVersion;
